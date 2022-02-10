fc3cc4a7a9ced06a0203ba73b57bebf2ef8c89eb
Stefano <stefano@elysiumstudios.se>
2022-02-10 17:51:16 +0200

    first commit

---
+++ generate.sh
@@ -0,0 +1,3 @@
+scons -o2
+scons platform=javascript tools=no target=release disable_3d=yes  disable_advanced_gui=yes -o2
+scons platform=javascript tools=no target=release_debug disable_3d=yes  disable_advanced_gui=yes
